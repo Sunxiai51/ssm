899aa1df2fed84d0a86cbc77b27747ab4acf3b4c
Sunxiai51 <wuyiyang1992@qq.com>
2017-12-19 14:52:56 +0800

    debug to test the <url-pattern> should be "/" or "/*", "/" seems right.

--- src/main/java/com/sunveee/template/ssm/controller/IndexController.java
+++ src/main/java/com/sunveee/template/ssm/controller/IndexController.java
@@ -1,14 +1,21 @@
 package com.sunveee.template.ssm.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sunveee.template.ssm.util.LogUtil;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping(value = "/")
     public String toIndex() {
+        LogUtil.debug(logger, "访问首页Controller");
+
         return "index";
     }
 
--- src/main/java/com/sunveee/template/ssm/controller/UserController.java
+++ src/main/java/com/sunveee/template/ssm/controller/UserController.java
@@ -2,30 +2,48 @@ package com.sunveee.template.ssm.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.fastjson.JSON;
 import com.sunveee.template.ssm.entity.mbg.User;
 import com.sunveee.template.ssm.service.UserService;
+import com.sunveee.template.ssm.util.LogUtil;
 import com.sunveee.template.ssm.util.PageHandler;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    private UserService userService;
+    private UserService         userService;
 
     @RequestMapping(value = "/list")
     public String toUserList(@RequestParam(value = "pageNo", defaultValue = "0") Integer pageNo, @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize, Model model) {
+        LogUtil.debug(logger, "访问用户列表Controller");
+
         List<User> userPage = userService.getUserPage(pageNo, pageSize);
         model.addAttribute("userPage", userPage);
         int infoCount = userService.getAllUserCount();
         PageHandler.handlePage(pageNo, pageSize, infoCount, model);
         return "user/user-list";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/id/{userId}")
+    public String toUserInfo(@PathVariable("userId") Integer userId) {
+        LogUtil.debug(logger, "根据用户id访问用户信息Controller");
+
+        User user = userService.getUserById(userId);
+        return JSON.toJSONString(user);
     }
 
 }
